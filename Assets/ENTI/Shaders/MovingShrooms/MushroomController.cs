ed2fbe7d5ce0ee82c704305be9a25e71ae353cda
RoheSama <roger.companys@estudiant.enti.cat>
2026-06-15 20:14:41 +0200

    Material applied

---
+++ Assets/ENTI/Shaders/MovingShrooms/MushroomController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class MushroomController : MonoBehaviour
+{
+    public Transform player;
+    public Renderer[] mushroomRenderers;
+
+    private static readonly int PlayerPositionID =
+        Shader.PropertyToID("_PlayerPosition");
+
+    // Cache del PropertyBlock para evitar GC alloc cada frame
+    private MaterialPropertyBlock _mpb;
+
+    private void Awake()
+    {
+        _mpb = new MaterialPropertyBlock();
+    }
+
+    private void Update()
+    {
+        if (player == null) return;
+
+        Vector3 playerPos = player.position;
+
+        foreach (Renderer rend in mushroomRenderers)
+        {
+            rend.GetPropertyBlock(_mpb);
+            _mpb.SetVector(PlayerPositionID, playerPos);
+            rend.SetPropertyBlock(_mpb);
+        }
+    }
+}
